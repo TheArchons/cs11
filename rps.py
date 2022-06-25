a8b7925ca34656af1f21ccb7650d04baec9aadfe
TheArchons <progamer9138@gmail.com>
2022-06-25 15:30:25 -0700

    update rps

--- rps.py
+++ rps.py
@@ -1,28 +1,29 @@
 import random
 
 def tieWinLose(player, computer):
-    if player == computer:
-        print("Tie!")
+    if player == computer: # both inputs are the same
+        print("Tie!") # tie
         return
-    if player == "rock":
-        if computer == "paper":
+    if player == "rock": # player picked rock
+        if computer == "paper": # computer picked paper
             print("You lose! {} beats {}".format(computer, player))
-        else:
+        else: # computer picked scissors
             print("You win! {} beats {}".format(player, computer))
-    if player == "paper":
-        if computer == "scissors":
+        # the computer couldn't have picked rock because that would've ended in a tie
+    if player == "paper": # player picked paper
+        if computer == "scissors": # computer picked scissors
             print("You lose! {} beats {}".format(computer, player))
-        else:
+        else: # computer picked rock 
             print("You win! {} beats {}".format(player, computer))
-    if player == "scissors":
-        if computer == "rock":
+    if player == "scissors": # player picked scissors
+        if computer == "rock": # computer picked rock
             print("You lose! {} beats {}".format(computer, player))
-        else:
+        else: # computer picked paper
             print("You win! {} beats {}".format(player, computer))
 
-rps = input('rock, paper, or scissors? ')
-aiRPS = random.choice(['rock', 'paper', 'scissors'])
-if rps == 'rock' or rps == 'paper' or rps == 'scissors':
-    tieWinLose(rps, aiRPS)
-else:
-    print("Invalid input. Pick rock, paper, or scissors.")
+rps = input('rock, paper, or scissors? ') # user picks rock, paper, or scissors
+aiRPS = random.choice(['rock', 'paper', 'scissors']) # ai picks using random.choice
+if rps == 'rock' or rps == 'paper' or rps == 'scissors': # if the user input is one the options
+    tieWinLose(rps, aiRPS) # determine the winner by calling the function tieWinLose
+else: # if input is invalid
+    print("Invalid input. Pick rock, paper, or scissors.") # print saying input is invalid
