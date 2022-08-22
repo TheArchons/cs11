26ac33a817efb5249d533ee939b6d6239c538574
TheArchons <progamer9138@gmail.com>
2022-08-21 17:58:59 -0700

    rework rpsProj

--- rpsProj.py
+++ rpsProj.py
@@ -1,34 +1,36 @@
-import random, os, json
+import random
+import os
+import json
+
 
 def determineWinner(playerChoice):
+    """Determines the winner of the game"""
     computerChoice = random.choice(['rock', 'paper', 'scissors']) # pick a random choice from the list
 
-    if playerChoice == computerChoice:
-        print('Tie! You both chose ' + playerChoice)
-        return 'tie'
-    elif playerChoice == 'rock':
-        if computerChoice == 'paper':
-            print('Computer wins! Paper beats rock.')
-            return 'computer'
-        else:
-            print('Player wins! Rock beats scissors.')
-            return 'player'
-    elif playerChoice == 'paper':
-        if computerChoice == 'scissors':
-            print('Computer wins! Scissors beats paper.')
-            return 'computer'
-        else:
-            print('Player wins! Paper beats rock.')
-            return 'player'
-    elif playerChoice == 'scissors':
-        if computerChoice == 'rock':
-            print('Computer wins! Rock beats scissors.')
-            return 'computer'
-        else:
-            print('Player wins! Scissors beats paper.')
-            return 'player'
+    matcher = {
+        'rock': {'rock': 'tie', 'paper': 'computer', 'scissors': 'player'},
+        'paper': {'rock': 'player', 'paper': 'tie', 'scissors': 'computer'},
+        'scissors': {'rock': 'computer', 'paper': 'player', 'scissors': 'tie'}
+    }
+
+    messageMatcher = {
+        'tie': 'Tie! you both chose {}'.format(playerChoice),
+        'player': 'You win! {} beats {}'.format(playerChoice, computerChoice),
+        'computer': 'You lose! {} beats {}'.format(computerChoice, playerChoice)
+    }
+
+    result = matcher[playerChoice][computerChoice]
+    print(messageMatcher[result])
+    return result
+
+
+def saveScores(playerScore, computerScore):
+    """Saves scores to a json file"""
+    with open('scores.json', 'w') as f:
+        json.dump({'player': playerScore, 'computer': computerScore}, f)
 
 # note: I used json over a txt file because it is easier to read and write
+
 
 if os.path.exists('rpsScores.json'): # load scores from the json file if it exists
     with open('rpsScores.json', 'r') as f:
@@ -44,19 +46,18 @@ while True:
 
     if userChoice == "exit": # if exit was entered, break the loop, quitting the program
         break
-    
+
     if userChoice == 'reset': # if reset was entered, reset the scores to 0 and continue the loop
         playerScore = 0
         computerScore = 0
-        with open('rpsScores.json', 'w') as f:
-            json.dump({'player': playerScore, 'computer': computerScore}, f)
+        saveScores(playerScore, computerScore)
         print('Scores reset to 0.', end='\n\n') # tell the user scores have been reset
         continue
 
     if userChoice not in ['rock', 'paper', 'scissors']: # if the user entered an invalid choice (not equal to rock, paper, or scissors), print an error message and continue the loop
         print("Invalid choice. Please try again.", end="\n\n") # 2 newlines to separate the error message from the next prompt
         continue
-    
+
     winner = determineWinner(userChoice) # determine the winner
 
     # add scores
@@ -65,11 +66,9 @@ while True:
     elif winner == "computer":
         computerScore += 1
 
-    # save scores to file.
     # Alternatively, you could save the score on exit by putting this code in the userChoice == 'exit' conditional.
     # However, this saves the score even if you quit the program without entering exit (for example, by pressing ctrl+c).
-    with open('rpsScores.json', 'w') as f:
-        json.dump({'player': playerScore, 'computer': computerScore}, f)
-    
+    saveScores(playerScore, computerScore)
+
     # print scores
     print('Player score: ' + str(playerScore) + ' Computer score: ' + str(computerScore), end='\n\n') # 2 newlines to separate the scores from the next round
